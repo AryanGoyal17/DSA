26aabd77d9371b47279ee700afe60217b47a5f7d
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-03-27 23:54:17 +0530

    Understanding basics of Space Complexity

---
+++ C++/Time and Space Complexity_Notes/02_Space_complexity.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //Space complexity = Auxiliary space + Input space
+    // Represented by big-O notation..
+
+    ////
+
+    //Example Q-- Take input of 2 integers a and b , and print the sum
+    //Answer -- c stores the sum and is printed..
+
+    //Then, a and b are the input space(Space that you take to store the input)
+    //And, c is the auxiliary space(Extra Space that you take to solve the problem)
+
+    //Space complexity -- O(3)
+
+    ////
+
+    //NOTE: if a array arr[n] is created then the space complexity -- O(N) [part of input space]
+
+
+    //IMPORTANT!
+    //In the question above, if we write b = a + b and print b then less space is consumed, but this should never be done
+    //as its a good practice to never tamper/change the data given unless specified otherwise..
+
+    
+//NOTE FOR CP--
+//Code written in platforms like leetcode, gfg, codeforces is sent to their servers which take around 1 second for 10^8 ops..
+//So if in a question -- Time limit given is 2s, then ensure that TC -- O(2*(10^8))
+
+
+    return 0;
+}
