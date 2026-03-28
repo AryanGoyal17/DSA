3eef04dbcfba3291748b09ac5b0f362ec704a0f5
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-03-28 15:06:01 +0530

    q2_solution

---
+++ C++/Pattern_Problems/02_q2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= i; j++){
+            cout << "*";
+        }
+      cout << endl;
+    }
+
+//Time complexity --- O(N(N+1)/2), approximate -- O(N^2)
+
+    return 0;
+}
