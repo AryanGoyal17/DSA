0662c1e6a9b29735338717e0b4f81e89cf24a0a7
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-03-28 15:03:22 +0530

    q1_solution

---
+++ C++/Pattern_Problems/01_q1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int N){
+
+        for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+}
+
+int main(){
+
+   int t = 3; //Running for 3 test cases..
+
+   for(int k = 0; k < t; k++){
+    int N;
+    cin >> N;
+    pattern(N);
+   }
+
+//Understanding test cases and online compiler done...
+//Time complexity -- O(N^2)
+
+    return 0;
+}
