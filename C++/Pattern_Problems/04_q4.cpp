a96f5f9dced944059dfb8797252235bf0e84b63a
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-03-28 15:18:08 +0530

    q4_solution

---
+++ C++/Pattern_Problems/04_q4.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int N){
+    
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= i; j++){
+           cout << i + 1;
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+
+    int N; 
+    cin >> N;
+    pattern(N);
+
+    //Time complexity --- O(N(N+1)/2), approximate -- O(N^2)
+    //Space complexity --- O(1)
+    //Method-2-- initialize i and j as 1 inside loop and print j instead of j + 1..This method is more readable however the space and time complexity remains the same..
+
+
+    return 0;
+}
