bda459df1771efbc41b5f03bd73155e43b10d3ea
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-04-04 12:54:14 +0530

    MultiMap(Container - 11)

---
+++ C++/STL/Containers and Iterators/11_multimap.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //Multi-map -- Same as map... but the keys are not unique..
+
+    // keys-- sorted, not unique
+    // values -- not unique..
+
+    //Similar to the relationship between set and multiset..
+
+    //Initializing --
+
+    multimap <int, int> mm1;
+
+    return 0;
+}
