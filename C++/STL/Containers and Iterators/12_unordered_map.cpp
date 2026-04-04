5c04d8c5b51f66264b8e94b76af4e61234e3ab1f
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-04-04 13:04:03 +0530

    Unordered map(Container-12) + emplace v/s insert

---
+++ C++/STL/Containers and Iterators/12_unordered_map.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //Similar to relationship between set and unordered set...
+
+    //keys -- unique, but not sorted..
+    //values -- not unique
+
+    //Initializing --
+
+    unordered_map <int, int> um1;
+
+    //[] can be used to access values....
+
+
+    //NOTE: Time complexity of almost all unordered map ops = O(1), constant time.. except in worst case(collision) -- O(N), linear..
+    //NOTE: Time complexity of almost all map ops -- O(logn)
+
+
+    //NOTE---- FOR ALL C++ DATA STRUCTURES-- 
+    // ------- emplace ops(emplace, emplace_back, emplace_front etc..) are faster than insert/push ops(push_back, insert, push_front, etc...)
+
+    return 0;
+}
