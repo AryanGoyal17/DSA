8f3b3659a18372774902c2953963e7426a144dfb
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-14 18:51:59 +0530

    q4_solution updated

--- C++/Pattern_Problems/04_q4.cpp
+++ C++/Pattern_Problems/04_q4.cpp
@@ -19,7 +19,7 @@ int main(){
 
     //Time complexity --- O(N(N+1)/2), approximate -- O(N^2)
     //Space complexity --- O(1)
-    //Method-2-- initialize i and j as 1 inside loop and print j instead of j + 1..This method is more readable however the space and time complexity remains the same..
+    //Method-2-- initialize i and j as 1 inside loop and print i instead of i + 1..This method is more readable however the space and time complexity remains the same..
 
 
     return 0;
