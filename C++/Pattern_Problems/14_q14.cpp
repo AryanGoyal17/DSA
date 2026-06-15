578adbf6772da12527f64825f71676eef6833b3a
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-15 18:59:44 +0530

    q14_solution[Important]

---
+++ C++/Pattern_Problems/14_q14.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int N){
+    for(int i = 0; i < N; i++){
+        for(char j = 'A'; j <= 'A' + i; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+
+    int N;
+    cin >> N;
+    pattern(N);
+
+    //Space Complexity -- O(1)
+    //Time Complexity -- O(N^2)[approx]
+
+    return 0;
+}
