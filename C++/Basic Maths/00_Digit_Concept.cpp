e8b65f622ea91ee2a64282272cbe9d7bda818916
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-16 19:33:53 +0530

    Concept -- Extraction of digits of a number

---
+++ C++/Basic Maths/00_Digit_Concept.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //1-- Extraction of digits
+
+
+    //Let x = 7798
+
+    //Process-----
+    //To find the last digit -- take modulus with 10
+    //To find the digit at tens place -- divide with 10(take the integer part) and then take modulus with 10
+    //Follow the process of tens place to get the digit at hundreds place and so on...
+    //Once you get 0, it shows that extraction is completed!
+
+    
+}
