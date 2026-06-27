27fc9632afff38d3cea801886b1e6a54b4afe0dd
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-27 17:22:45 +0530

    Notes -- Basics of recursion

---
+++ Recursion/Basic_problems/00_notes.cpp
@@ -0,0 +1,13 @@
+//1---Recursion
+///// -- a fn calling itself, until a specified condition is met
+
+//2---Infinite Recursion
+
+///// infinite recursion --- stop condition is not present
+///// --- in case of infinite recursion, the process occurs until me run out of memory(segmentation fault) = Stack Overflow
+///// Stack overflow -- error which occurs when multiple fn calls occur during infnite recursion(we run out of memory available)
+
+//3---Stop Condition = base condition
+
+///// condition which tells when to stop calling fn's recursively(prevents stack overflow and segmentation fault)
+///// There can be multiple base conditions at one time..
