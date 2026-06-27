992b1be54700b5fac4bb51a99fcd14559508284b
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-27 23:46:55 +0530

    Printing abc N times -- method-3 + tc and sc of all methods

---
+++ Recursion/Basic_problems/01_q1_m3.cpp
@@ -0,0 +1,29 @@
+//Understand recursion by printing something(abc) N times
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void fn1(int i, int N){
+
+    if(i > N) return;
+
+    cout << "abc" << endl;
+
+    fn1(i + 1, N);
+}
+
+int main(){
+
+    int N;
+    cout << "Enter N: ";
+    cin >> N;
+    
+    fn1(1, N);
+
+    return 0;
+
+    //Time complexity -- O(N) -- n function calls
+    //Space complexity -- O(N) -- n function calls waiting inside stack space -- call stack of N fn's waiting created
+
+    //Time complexity of m1,m2,m3 same as above
+}
