9f74f8b6b7674dde1c0666871323ef958686977b
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-27 23:22:58 +0530

    Printing abc N times -- method-1

---
+++ Recursion/Basic_problems/01_q1_m1.cpp
@@ -0,0 +1,27 @@
+//Understand recursion by printing something(abc) N times
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int cnt = 0;
+
+void fn1(int N){
+
+    if(cnt == N) return;
+
+    cout << "abc" << endl;
+    cnt++;
+
+    fn1(N);
+}
+
+int main(){
+
+    int N;
+    cout << "Enter N: ";
+    cin >> N;
+    
+    fn1(N);
+
+    return 0;
+}
