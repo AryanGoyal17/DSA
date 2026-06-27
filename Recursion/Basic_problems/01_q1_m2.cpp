8662aa9f2e11da63d2c37741117736c4d28bb046
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-27 23:27:08 +0530

    Printing abc N times -- method-2(better)

---
+++ Recursion/Basic_problems/01_q1_m2.cpp
@@ -0,0 +1,26 @@
+//Understand recursion by printing something(abc) N times
+
+//NOTE: Dont use global variables while solving on leetcode
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void fn1(int N){
+
+    if(N <= 0) return;
+
+    cout << "abc" << endl;
+
+    fn1(N-1);
+}
+
+int main(){
+
+    int N;
+    cout << "Enter N: ";
+    cin >> N;
+    
+    fn1(N);
+
+    return 0;
+}
