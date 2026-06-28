53d48af00c9a7aca5c8f221c7d818bb24a393d32
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-28 18:08:46 +0530

    updated notes of recursion basics

--- Recursion/Basic_problems/00_notes.cpp
+++ Recursion/Basic_problems/00_notes.cpp
@@ -17,9 +17,22 @@
 
 ///// explain recursion code in terms of recursion tree in notes
 
+//5--- Recursive call
+
+///// specific line of code inside a function where the function invokes (calls) itself.
+
+//6--- Types of recursion -- head and tail
+
+///// head recursion ---- printing happens after the recursive call is executed
+///// tail recursion ---- printing happens before the recursive call is executed
+
+//// For now treat head recursion the same as backtracking
+
 //summary -----
 
 //1-- what is recursion
 //2-- base case/condition
 //3-- stack overflow and stack space
 //4-- recursion tree
+//5-- recursive call
+//6-- types of recursion and note on backtracking
